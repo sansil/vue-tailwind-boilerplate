451a084230e21ba4691fd938b5ef8dc4d1af013c
sansil <sansil32@gmail.com>
2020-05-04 20:15:57 -0300

    first commit, minimal setup

---
+++ tailwind.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    })
+  ]
+}
